05a826b2ba1c5488655f0edcabf9654fd230c0f9
Andres Martinez <andres-martinez@uiowa.edu>
2022-03-11 13:56:20 -0600

    modify equations

--- TotalPCBFluxCalculation.R
+++ TotalPCBFluxCalculation.R
@@ -88,7 +88,6 @@ pars[157:159,2] <- c(465.740544)
 pars[160,2] <- c(498.64)
 
 # Add ortho Cl of individual PCB congeners
-
 pars[,3] <- c(1, 0, 0, 2, 1, 1, 1, 1, 1, 2, 0, 0, 0, 2,
               2, 2, 3, 1, 1, 1, 1, 2, 1, 1, 2, 1, 2, 1,
               0, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 2, 2, 3,
@@ -163,14 +162,13 @@ Kow.error <- pars$Kow.error
 
 # Flux calculations -------------------------------------------------------
 
-final.result = function(MW.PCB, H0.mean, H0.error, 
+final.result = function(MW.PCB, H0.mean, H0.error, Kow.mean, Kow.error,
                         C.PCB.water.mean, C.PCB.water.error,
                         C.PCB.air.mean, C.PCB.air.error, nOrtho.Cl) {
   # fixed parameters
   
   R = 8.3144
   T = 298.15
-  w = 3
   
   F.PCB.aw <- NULL
   for (replication in 1:1000) {
@@ -180,22 +178,35 @@ final.result = function(MW.PCB, H0.mean, H0.error,
     a <- rnorm(1, 0.085, 0.007)
     b <- rnorm(1, 1, 0.5)
     c <- rnorm(1, 32.7, 1.6)
+    a2 <- rnorm(1, 0.13, 0.02)
+    b2 <- rnorm(1, 2.9, 1.2)
+    c2 <- rnorm(1, 47.8, 4.3)
+    e <- abs(rnorm(1, e.mean, e.error))
+    n <- abs(rnorm(1, n.mean, n.error))
     H0 <- rnorm(1, H0.mean, H0.error)
-    P <- rnorm(1, P.mean, P.error)					
-    u <- abs(rnorm(1, u.mean, u.error)) #m/s
-    C.PCB.water <- rnorm(1, C.PCB.water.mean, C.PCB.water.error) #ng/L
-    C.PCB.air <- rnorm(1, C.PCB.air.mean, C.PCB.air.error) #ng/m3
-    T.water <- rnorm(1, T.water.mean, T.water.error) #C 
-    T.air <- rnorm(1, T.air.mean, T.air.error) #C
-    Q <- abs(rnorm(1, Q.mean, Q.error))
-    h <- abs(rnorm(1, h.mean, h.error)) #m
+    Kow <- rnorm(1, Kow.mean, Kow.error)
+    P <- rnorm(1, P.mean, P.error)
+    u <- rweibull(1, shape = e, scale = n) #m/s
+    C.PCB.water <- abs(rnorm(1, C.PCB.water.mean, C.PCB.water.error)) #ng/m3
+    C.PCB.air <- abs(rnorm(1, C.PCB.air.mean, C.PCB.air.error)) #pg/m3
+    DOC <- abs(rnorm(1, 1.5, 0.375)) #mg/L error = 25%
+    T.water <- rnorm(1, twater.mean, twater.error) #C 
+    T.air <- rnorm(1, tair.mean, tair.error) #C
     
     # computed values
     
     DeltaUaw <- (a*MW.PCB-b*nOrtho.Cl+c)*1000
     K <- 10^(H0)*101325/(R*T)
-    K.air.water <- K*exp(-DeltaUaw/R*(1/(T.water+273.15)-1/T))
+    K.air.water <- K*exp(-(DeltaUaw/R)*(1/(T.water+273.15)-1/T))
     K.final <- K.air.water*(T.water+273.15)/(T.air+273.15) # no units
+    
+    DeltaUoa <- (-a2*MW.PCB+b2*nOrtho.Cl-c2)*1000
+    DeltaUow <- DeltaUoa + DeltaUaw
+    
+    Kow.water.t <- 10^(Kow)*exp(-(DeltaUow/R)*(1/(T.water+273.15)-1/T))
+    Kdoc.t <- 0.06*Kow.water.t
+    
+    C.PCB.water.f <- C.PCB.water/(1+Kdoc.t*DOC/1000^2)#ng/m3
     
     D.water.air <- 10^(-3)*1013.25*((273.15+T.air)^1.75*((1/28.97)+(1/18.0152))^(0.5))/P/(20.1^(1/3)+9.5^(1/3))^2
     D.PCB.air <- D.water.air*(MW.PCB/18.0152)^(-0.5)
@@ -208,29 +219,28 @@ final.result = function(MW.PCB, H0.mean, H0.error,
     diff.co2 <- 0.05019*exp(-19.51*1000/(273.15+T.water)/R)
     D.PCB.water <- diff.co2*(MW.PCB/44.0094)^(-0.5)
     Sc.PCB.water <- v.water/D.PCB.water
-    
-    # Two methods to determine k600. K600 = f(flow or wind).
-    # For the particular conditions of the sampling location,
-    # a better estimate is provided by the flow and not the wind speed
-    # see k600 script
-    
-    # i) flow
-    
     Sc.co2.water <- v.water/diff.co2
-    Flow.veloc <- Q*0.02/(w*h)
-    k600 <- 1.72*(Flow.veloc*100/h)^(0.5) #cm/h
-    V.PCB.water = k600/3600*(Sc.PCB.water/Sc.co2.water)^(-0.5) #cm/s
+    
+    integrand <-function(u) {
+      ((e/u)*(u/n)^e)*exp(-(u/n)^e)*(0.000125*u^1.64) # cm/s
+      }
+    
+    k600 <- integrate(integrand, lower = 0, upper = Inf) #cm/s
+    
+    if(u > 5){
+      V.PCB.water <- k600$value*(Sc.PCB.water/Sc.co2.water)^(-0.5)  
+    } else {
+      V.PCB.water <- k600$value*(Sc.PCB.water/Sc.co2.water)^(-2/3)
+    }
+    
     mtc.PCB <- ((1/V.PCB.water+1/(V.PCB.air*K.final)))^(-1) #cm/s
-    F.PCB.aw <- c(F.PCB.aw, mtc.PCB*(C.PCB.water-C.PCB.air/K.final/1000)*3600*24*10) #pg/m2/d
+    
+    F.PCB.aw <- c(F.PCB.aw, mtc.PCB*(C.PCB.water.f - C.PCB.air/K.final/1000)*86400/100) #ng/m2/d
     
   }
   
-  mmm <- mean(F.PCB.aw)	#pg/m2/day
-  sss <- sd(F.PCB.aw)	#pg/m2/day
-  q2.5 <- quantile(F.PCB.aw, 0.025) #pg/m2/day
-  q97.5 <- quantile(F.PCB.aw, 0.975) #pg/m2/day
+  F.PCB.aw
   
-  c(mmm, sss, q2.5, q97.5)
 }
 
 C.PCB.water.mean <- as.numeric(pars.water.4/1000) # 1000 to get ng/L from pg/L
