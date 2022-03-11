05da60cef0c8843ed8d00bb66087057978fae56e
Andres Martinez <andres-martinez@uiowa.edu>
2022-03-11 17:04:20 -0600

    add note in replication

--- TotalPCBFluxCalculation.R
+++ TotalPCBFluxCalculation.R
@@ -185,7 +185,7 @@ final.result = function(MW.PCB, H0.mean, H0.error, Kow.mean, Kow.error,
   T = 298.15
   
   F.PCB.aw <- NULL
-  for (replication in 1:100) {
+  for (replication in 1:100) { # change replication
     
     # random parameters
     
